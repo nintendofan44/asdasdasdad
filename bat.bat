c0eb4372cd8d90d9946c78bd08df3051cd7edb7f
nintendofan44 <85742902+nintendofan44@users.noreply.github.com>
2021-11-08 20:43:17 -0300

    Create bat.bat

---
+++ bat.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0a
+:A
+echo 7 f s o 0 s 2 7 A c 9 2 D 4 h H o q 5 Q O P 9 s o 1 s c x s 1 l 3 d q 5 o l p a S d 2 8 a p i 3 9 8 d 2 k i p q d z 9 2 7 A d k i q p i 3 9 J 2 3 9 s d a
+ping localhost -n 1>nul
+echo i s 8 0 A X S k p q o s 8 3 8 s a q P C a 2 1 c o d i l P 0 8 4 x 8 s p a l c x z p q e d v 0 3 9 D F 3 1 f S 3 v c s a w a e v c x 2 4 f a j i k l p o 8 h
+ping localhost -n 1>nul 
+echo  o d i l P 0 8 4 x 8 s p a l c x z p q e d v o s 8 3 8 s a q P C a 2 1 c o d i l P 0 8 4 x 8 s p a l c x 3 1 f S 3 v c o d i l P 0 8 4 x 8 s p 3 1 f S 3 a j i
+ping localhost -n 1>nul A c 9 2 D 4 h H o q  1 c o d i l P 0  o d i l P 0 0 8 4 x 8 s p a l c x z p q e d 2 8 a p i 3 9 8 d  x 8 s p a l c  z 9 2 7 A d k i q p i 
+goto A
